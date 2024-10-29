f9c90bc3b46c2eb8ce6c9ee413b121799536f903
paragoncodings <paragoncodings@gmail.com>
2024-10-29 10:37:31 +0500

    change in vite

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,25 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/'
+})
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode
-  const env = loadEnv(mode, process.cwd());
+// export default defineConfig(({ mode }) => {
+//   // Load environment variables based on the current mode
+//   const env = loadEnv(mode, process.cwd());
 
-  return {
-    plugins: [react()],
-    define: {
-      // Make sure you expose only VITE_ prefixed variables
-      'process.env': {
-        VITE_APP_API_BASE_URL: env.VITE_APP_API_BASE_URL,
-      },
-    },
-  };
-});
+//   return {
+//     plugins: [react()],
+//     define: {
+//       // Make sure you expose only VITE_ prefixed variables
+//       'process.env': {
+//         VITE_APP_API_BASE_URL: env.VITE_APP_API_BASE_URL,
+//       },
+//     },
+//   };
+// });
