ec8ac2d634d4dd5426c43d26c9554b441b212b99
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-10-29 10:17:23 +0500

    switch to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import {  BrowserRouter as Router} from 'react-router-dom';
+// import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import CreateContext from './Context/CreateContext';
