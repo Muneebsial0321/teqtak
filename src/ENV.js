0ee9f0086cea1fe25582995a37d6ff29f62f495f
mabdulrahman176 <paragoncodings@gmail.com>
2024-11-13 16:22:52 +0500

    bug fixed

--- src/ENV.js
+++ src/ENV.js
@@ -1,4 +1,2 @@
-// export const REACT_APP_API_BASE_URL= 'https dev://api.teqtak.com'
-export const REACT_APP_API_BASE_URL_= 'http://localhost:5000'
 export const REACT_APP_API_BASE_URL= 'https://api.teqtak.com'
 // export const REACT_APP_API_BASE_URL= 'http://localhost:5000'
