45f21758edddbdcd3a4175f57fea8ddab4cb2ff7
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-11-15 10:31:16 +0500

    landing page bug fixed

--- src/LandingPage/Header.jsx
+++ src/LandingPage/Header.jsx
@@ -1,4 +1,4 @@
-import{ useState } from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { IoMenu } from 'react-icons/io5';
 import HeaderComponent from './HeaderComponent';
@@ -31,91 +31,95 @@ const Header = () => {
 
 
   return (
-    <div className='overflow-x-hidden'>
-    <div className="flex bg-[#e4efff] justify-between h-36 w-full px-6 sm:px-6 md:px-12 lg:px-28 pt-4 relative items-center ">
-      {/* Logo */}
-      <div className="Logo">
-        <NavLink to="/" className="text-[#6165f3] font-sans text-2xl sm:text-2xl font-extrabold">
-          Investor App
-        </NavLink>
-      </div>
+    <>
+      <div className='overflow-x-hidden bg-[#e4efff]'>
 
-      {/* Desktop Menu */}
-      <ul className="hidden md:flex justify-evenly w-3/5 lg:w-2/5 text-lg text-center">
-        <NavLink to="/" className="text-gray-800 font-bold">
-          Home
-        </NavLink>
-        <NavLink to="/about" className="text-gray-400">
-          About App
-        </NavLink>
-        <NavLink to="/faqs" className="text-gray-400">
-          FAQs
-        </NavLink>
-        <NavLink to="/preview" className="text-gray-400">
-          Preview
-        </NavLink>
-      </ul>
+        <div className="flex  z-20 justify-between h-36 w-full px-6 sm:px-6 md:px-12 lg:px-28 pt-4 relative items-center ">
+          {/* Logo */}
+          <div className="Logo">
+            <NavLink to="/" className="text-[#6165f3] font-sans text-2xl sm:text-2xl font-extrabold">
+              Investor App
+            </NavLink>
+          </div>
 
-      {/* Download Button */}
-      <button className="hidden md:block bg-white rounded h-10 w-28 px-4 py-2 text-[#6165F3] border-2 z-10">
-        Download
-      </button>
-
-      {/* Toggle Button for Mobile */}
-      <button
-        className="md:hidden text-2xl"
-        onClick={() => setNavToggle(!navToggle)}
-      >
-        <IoMenu />
-      </button>
-
-      {/* Toggle Menu for Mobile */}
-      {navToggle && (
-        <div
-          ref={menuRef}
-          className="absolute top-16 right-0 w-48 bg-white shadow-lg z-20 md:hidden"
-        >
-          <ul className="flex flex-col items-start p-4">
-            <NavLink
-              to="/"
-              className="text-gray-800 block py-2 w-full"
-              onClick={() => setNavToggle(false)}
-            >
+          {/* Desktop Menu */}
+          <ul className="hidden md:flex justify-evenly w-3/5 lg:w-2/5 text-lg text-center">
+            <NavLink to="/" className="text-gray-800 font-bold">
               Home
             </NavLink>
-            <NavLink
-              to="/about"
-              className="text-gray-400 block py-2 w-full"
-              onClick={() => setNavToggle(false)}
-            >
+            <NavLink to="/about" className="text-gray-400">
               About App
             </NavLink>
-            <NavLink
-              to="/faqs"
-              className="text-gray-400 block py-2 w-full"
-              onClick={() => setNavToggle(false)}
-            >
+            <NavLink to="/faqs" className="text-gray-400">
               FAQs
             </NavLink>
-            <NavLink
-              to="/preview"
-              className="text-gray-400 block py-2 w-full"
-              onClick={() => setNavToggle(false)}
-            >
+            <NavLink to="/preview" className="text-gray-400">
               Preview
             </NavLink>
           </ul>
+
+          {/* Download Button */}
+          <button className="hidden md:block bg-white rounded h-10 w-28 px-4 py-2 text-[#6165F3] border-2 z-10">
+            Download
+          </button>
+
+          {/* Toggle Button for Mobile */}
+          <button
+            className="md:hidden text-2xl"
+            onClick={() => setNavToggle(!navToggle)}
+          >
+            <IoMenu />
+          </button>
+
+          {/* Toggle Menu for Mobile */}
+          {navToggle && (
+            <div
+              ref={menuRef}
+              className="absolute top-16 right-0 w-48 bg-white shadow-lg z-20 md:hidden"
+            >
+              <ul className="flex flex-col items-start p-4">
+                <NavLink
+                  to="/"
+                  className="text-gray-800 block py-2 w-full"
+                  onClick={() => setNavToggle(false)}
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to="/about"
+                  className="text-gray-400 block py-2 w-full"
+                  onClick={() => setNavToggle(false)}
+                >
+                  About App
+                </NavLink>
+                <NavLink
+                  to="/faqs"
+                  className="text-gray-400 block py-2 w-full"
+                  onClick={() => setNavToggle(false)}
+                >
+                  FAQs
+                </NavLink>
+                <NavLink
+                  to="/preview"
+                  className="text-gray-400 block py-2 w-full"
+                  onClick={() => setNavToggle(false)}
+                >
+                  Preview
+                </NavLink>
+              </ul>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-  
-    <HeaderComponent/>
-    {/* <Second/> */}
-     <Third/>
-    {/* <Forth/>  */}
-    {/* <ThreeAfter/> don't 😥😣 uncomment*/}
-    <Fifth/> 
-</div>
+      </div>
+
+      <HeaderComponent />
+      <Second />
+      <Third />
+      <Forth />
+      {/* <ThreeAfter/> don't 😥😣 uncomment*/}
+      <Fifth />
+
+    </>
   );
 };
 
